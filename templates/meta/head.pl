6a7f565913a6bfff17e3cda8b1c59c1812dca840
nyarla <nyarla@kalaclista.com>
2022-07-12 17:00:58 +0900

    refactor(archives): rewrite templates for migrate builder to latest

--- templates/meta/head.pl
+++ templates/meta/head.pl
@@ -1,13 +1,16 @@
-my $budoux    = "https://cdn.skypack.dev/budoux";
-my $normalize = "https://cdn.skypack.dev/normalize.css";
-my $tables    = {
+my $budoux    = 'https://cdn.skypack.dev/budoux';
+my $normalize = 'https://cdn.skypack.dev/normalize.css';
+
+my $tables = {
   posts => [ 'Blog',    'BlogPosting' ],
   echos => [ 'Blog',    'BlogPosting' ],
   notes => [ 'WebSite', 'Article' ],
   pages => [ 'WebSite', 'WebPage' ],
 };
 
-my $types = sub {
+no warnings 'redefine';
+
+sub types {
   my ( $kind, $section ) = @_;
 
   if ( $kind eq 'permalink' ) {
@@ -24,7 +27,19 @@ my $types = sub {
   }
 
   return $tables->{'pages'}->[0];
-};
+}
+
+sub item {
+  my ( $item, $href, $type ) = @_;
+
+  my %attr;
+  @attr{qw( rel href )} = ( $item, $href );
+  $attr{'type'} = $type if ( defined $type );
+
+  return link_( \%attr );
+}
+
+use warnings 'redefine';
 
 my $author = {
   '@type' => 'Person',
@@ -45,53 +60,57 @@ my $publisher = {
 };
 
 my $head = sub {
-  my ( $vars, $baseURI, $data ) = @_;
+  my ( $info, $baseURI ) = @_;
+  my ( $title, $website, $description, $section, $kind, $home, $permalink,
+    $tree )
+    = @{$info}
+    {qw( title website description section kind home permalink tree )};
+  my $parent = $tree->[ $tree->@* - 2 ]->{'href'};
+  my $avatar = href( '/assets/avatar.png', $baseURI );
 
-  # internal functions
-  # ==================
-  my $href =
-    sub { my $link = $baseURI->clone; $link->path( $_[0] ); $link->as_string };
+  # prefetch and preload
+  my $dns =
+    meta( { 'http-equiv' => 'x-dns-prefetch-control', content => 'on' } );
+  my $prefetch =
+    link_( { rel => 'dns-prefetch', href => 'https://cdn.skypack.dev/' } );
+  my @preloads = (
+    link_( { rel => 'preload', href => $budoux,    as => 'script' } ),
+    link_( { rel => 'preload', href => $normalize, as => 'style' } ),
+  );
 
-  # global variables
-  # ================
-  my $title       = $data->{'title'};
-  my $website     = $data->{'website'};
-  my $description = $data->{'description'};
-  my $avatar      = $href->("/assets/avatar.png");
-  my $section     = $data->{'section'};
-  my $kind        = $data->{'kind'};
-  my $home        = $data->{'home'};
-  my $permalink   = $data->{'permalink'};
-  my $tree        = $data->{'tree'};
-  my $parent      = $tree->[ $tree->@* - 2 ]->{'href'};
+  my $script =
+    script( { src => href( '/assets/script.js', $baseURI ), type => 'module' },
+    "" );
+  my @css = (
+    link_(
+      {
+        rel  => 'stylesheet',
+        href => $normalize,
+      }
+    ),
+    link_(
+      {
+        rel  => 'stylesheet',
+        href => href( '/assets/stylesheet.css', $baseURI )
+      }
+    )
+  );
 
-  # elements
-  # ========
-
-  # basic
-  # -----
-  my $doctitle =
-    title( ( $title eq $website ) ? $title : "${title} - ${website}" );
-  my $charset  = meta( { charset => "utf-8" } );
-  my $viewport = meta(
+  # document format
+  my $charset   = meta( { charset => 'utf-8' } );
+  my $canonical = link_( { rel => 'canonical', href => $permalink } );
+  my $viewport  = meta(
     {
       name    => 'viewport',
       content => 'width=device-width,minimum-scale=1,initial-scale=1'
     }
   );
-  my $canonical = link_( { rel => "canonical", href => $permalink } );
 
-  # preload
-  # -------
-  my $prefetch =
-    link_( { rel => "dns-prefetch", href => "https://cdn.skypack.dev/" } );
-  my @preloads = (
-    link_( { rel => "preload", href => $budoux,    as => "script" } ),
-    link_( { rel => "preload", href => $normalize, as => "style" } ),
-  );
+  # document information
+  my $docname =
+    title( ( $title eq $website ) ? $title : "${title} - ${website}" );
+  my $docdesc = meta( { name => 'description', content => $description } );
 
-  # ogp
-  # ---
   my @ogp = (
     property( 'og:title',       $title ),
     property( 'og:site_name',   $website ),
@@ -101,8 +120,6 @@ my $head = sub {
     property( 'og:description', $description ),
   );
 
-  # twitter card
-  # ------------
   my @twitter = (
     data_( 'twitter:card',        'summary' ),
     data_( 'twitter:site',        '@kalaclista' ),
@@ -111,89 +128,51 @@ my $head = sub {
     data_( 'twitter:image',       $avatar ),
   );
 
-  # feeds
-  # -----
-  my $rss = feed(
-    $website . "の RSS フィード",
-    $href->("/${section}/index.xml"),
-    "application/rss+xml"
-  );
-  my $atom = feed(
-    $website . "の Atom フィード",
-    $href->("/${section}/atom.xml"),
-    "application/atom+xml"
-  );
-  my $jsonfeed = feed(
-    $website . "の JSON フィード",
-    $href->("/${section}/jsonfeed.json"),
-    "application/feed+json"
-  );
-
-  # JSON Linked Data
-  # ----------------
-  my $jsonld = script(
-    { type => 'application/ld+json' },
-    jsonld(
-      {
-        title     => $title,
-        href      => $permalink,
-        type      => $types->( $kind, $section ),
-        author    => $author,
-        publisher => $publisher,
-        image     => $avatar,
-        parent    => $parent,
-      },
-      $tree->@*,
-    )
+  # document metadata
+  my @feeds = (
+    feed(
+      "${website}の RSS フィード",
+      href( "/${section}/index.xml", $baseURI ),
+      "application/rss+xml"
+    ),
+    feed(
+      "${website}の Atom フィード",
+      href( "/${section}/atom.xml", $baseURI ),
+      "application/atom+xml"
+    ),
+    feed(
+      "${website}の JSON フィード",
+      href( "/${section}/jsonfeed.json", $baseURI ),
+      "application/feed+json"
+    ),
   );
 
-  # style and script
-  # ----------------
-  my $script =
-    script( { src => $href->('/assets/script.js'), type => 'module' }, "" );
-  my $css = link_( { rel => 'stylesheet', href => $normalize } );
-  my $style =
-    link_( { rel => 'stylesheet', href => $href->('assets/stylesheet.css') } );
+  my %jsonld;
+  @jsonld{qw( title href type author publisher image parent )} = (
+    $title,  $permalink, types( $kind, $section ),
+    $author, $publisher, $avatar, $parent
+  );
+  my $jsonld =
+    script( { type => 'application/ld+json' }, jsonld( \%jsonld, $tree->@* ), );
 
-  # assets
-  # ------
   my $webmanifest =
-    link_( { rel => "manifest", href => $href->("/manifest.webmanifest") } );
-  my $favicon = link_( { rel => "icon", href => $href->("/favicon.ico") } );
-  my $svgicon = link_(
-    { rel => "icon", href => $href->("/icon.svg"), type => "image/svg+xml" } );
-  my $appleicon = link_(
-    { rel => "apple-touch-icon", href => $href->('/apple-touch-icon.png') } );
+    item( manifest => href( '/manifest.webmanifest', $baseURI ) );
+  my $favicon = item( icon => href( '/favicon.ico', $baseURI ) );
+  my $svgicon = item( icon => href( '/icon.svg', $baseURI ), 'image/svg+xml' );
+  my $apple =
+    item( 'apple-touch-icon', href( '/apple-touch-icon.png', $baseURI ) );
 
-  # output
-  # ======
   return head(
     $charset,
+    $dns, $prefetch, @preloads,
 
-    $doctitle,
-    meta( { name => "description", content => $description } ),
-    $canonical,
-    @ogp,
-    @twitter,
-    $jsonld,
+    $script,    @css,
+    $canonical, $viewport,
 
-    $rss,
-    $atom,
-    $jsonfeed,
+    $docname, $docdesc, @ogp, @twitter,
+    $jsonld,  @feeds,
 
-    $webmanifest,
-    $favicon,
-    $svgicon,
-    $appleicon,
-
-    meta( { 'http-equiv' => 'x-dns-prefetch-control', content => 'on' } ),
-    $prefetch,
-    @preloads,
-
-    $viewport,
-    $script,
-    $css,
-    $style,
+    $webmanifest, $favicon, $svgicon, $apple,
   );
 };
 
--- templates/pages/archives.pl
+++ templates/pages/archives.pl
@@ -1,4 +1,4 @@
-my $content = sub {
+my $_content = sub {
   my $vars    = shift;
   my $baseURI = shift;
 
@@ -70,99 +70,133 @@ my $content = sub {
   );
 };
 
-my $template = sub {
-  my $vars    = shift;
-  my $baseURI = shift;
-  my $dir     = shift;
+my $content = sub {
+  my ( $vars, $baseURI ) = @_;
 
-  # internal functions
-  my $href =
-    sub { my $link = $baseURI->clone; $link->path( $_[0] ); $link->as_string };
-  my $head =
-    load( "Kalaclista::Head", $dir->child('../meta/head.pl')->stringify );
+  my ( $data, $entries, $year, $section ) =
+    @{$vars}{qw( data entries year section )};
 
-  # per page variable
-  my @entries = $vars->{'entries'}->@*;
-  my $year    = $vars->{'year'} // 0;
-
-  # local variables
-  my $title;
-  my $website = $vars->{'data'}->{'title'};
-  my $description;
-  my $section = $vars->{'section'};
-  my $kind    = $vars->{'kind'};
-  my $home    = $vars->{'home'};
-  my $permalink;
-  my $tree = [];
+  my @contents;
 
   if ( $section eq 'notes' ) {
-    $title       = $vars->{'data'}->{'title'};
-    $permalink   = $href->("/${section}/");
-    $description = $vars->{'data'}->{'summary'};
+    my @archives;
+    for my $meta ( sort { $b->lastmod cmp $a->lastmod } $entries->@* ) {
+      my $date = date( $meta->date );
+      push @archives,
+        li(
+        time_( { datetime => $date } ),
+        "${date}：",
+        a( { href => $meta->href->as_string, class => 'title' }, $meta->title )
+        );
+    }
+
+    @contents = ul( { class => 'archives' }, @archives );
+  }
+  else {
+    my @archives;
+    for my $meta ( sort { $b->date cmp $a->date } $entries->@* ) {
+      my $date = date( $meta->date );
+      push @archives,
+        li(
+        time_( { datetime => $date }, "${date}：" ),
+        a( { href => $meta->href->as_string, class => 'title' }, $meta->title )
+        );
+    }
+
+    my @years;
+    for my $yr ( sort { $b <=> $a }
+      $data->{'begin'} .. ( (localtime)[5] + 1900 ) )
+    {
+      if ( $yr == $year ) {
+        push @years, strong($year);
+        next;
+      }
+
+      push @years, a( { href => href( "/${section}/${yr}/", $baseURI ) }, $yr );
+    }
+
+    @contents = (
+      strong("${year}年："), ul( { class => 'archives' }, @archives ),
+      hr(),                p( "過去ログ：", ( join q{ / }, @years ) )
+    );
+  }
+
+  return main(
+    article(
+      {
+        class => [qw(entry entry__archives)]
+      },
+      header(
+        h1(
+          a( { href => href( "/${section}/", $baseURI ) }, $data->{'title'} )
+        )
+      ),
+      section(
+        { className( 'entry', 'content' ) }, p( $data->{'summary'} ),
+        hr(),                                @contents,
+      ),
+    )
+  );
+};
+
+my $template = sub {
+  my ( $vars, $baseURI ) = @_;
+
+  # vars of pages and archive
+  my $year = $vars->{'year'} // 0;
+
+  # vars of metadata
+  my $title;
+  my $description;
+  my $permalink;
+  my @tree;
+
+  my ( $data, $section, $kind, $home ) = @{$vars}{qw(data section kind home)};
+  my $website = $data->{'title'};
+
+  if ( $home || $section eq 'notes' ) {
+    ( $title, $description ) = @{$data}{qw(title summary)};
+    $permalink = href( "/${section}/", $baseURI );
   }
   else {
     $title       = sprintf "%04d年の記事一覧", $year;
-    $permalink   = $href->("/${section}/${year}/");
     $description = "${website}の${title}です";
+    $permalink   = href( "/${section}/${year}/", $baseURI );
   }
 
-  if ( $vars->{'home'} ) {
-    $title       = $vars->{'data'}->{'title'};
-    $description = $vars->{'data'}->{'summary'};
-    $permalink   = $href->("/${section}/");
-  }
-
-  push $tree->@*,
+  push @tree,
     +{
     name => 'カラクリスタ',
-    href => $href->('/'),
+    href => href( '/', $baseURI ),
     };
 
-  push $tree->@*,
+  push @tree,
     +{
     name => $website,
-    href => $href->("/${section}/"),
+    href => href( "/${section}/", $baseURI ),
     };
 
-  if ( $kind eq q{archive} ) {
-    push $tree->@*,
+  if ( !$home ) {
+    push @tree,
       +{
       name => $title,
       href => $permalink,
       };
   }
 
-  my $data = {
-    title       => $title,
-    website     => $website,
-    description => $description,
-    section     => $section,
-    kind        => $kind,
-    home        => $home,
-    permalink   => $permalink,
-    tree        => $tree,
-  };
+  my %info = ();
+  @info{qw( title website description section kind home permalink tree )} = (
+    $title, $website, $description, $section, $kind, $home, $permalink, \@tree
+  );
 
   return document(
-    $head->( $vars, $baseURI, $data ),
+    expand( 'meta/head.pl', \%info, $baseURI ),
     [
-      (
-        load( "Kalaclista::Title", $dir->child("../widgets/title.pl") )
-          ->( $vars, $baseURI, $data )
-      ),
-      (
-        load( "Kalaclista::Profile", $dir->child("../widgets/profile.pl") )
-          ->( $vars, $baseURI, $data )
-      ),
-      (
-        load( "Kalaclista::Menu", $dir->child("../widgets/menu.pl") )
-          ->( $vars, $baseURI, $data )
-      ),
+      expand( 'widgets/title.pl',   $vars, $baseURI ),
+      expand( 'widgets/profile.pl', $vars, $baseURI ),
+      expand( 'widgets/menu.pl',    $vars, $baseURI ),
       $content->( $vars, $baseURI ),
-      (
-        load( "Kalaclista::Info", $dir->child("../widgets/info.pl") )
-          ->( $vars, $baseURI, $data )
-      ),
+      expand( 'widgets/info.pl', $vars, $baseURI )
     ]
   );
 };
