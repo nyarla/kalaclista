c7bec74e50b558bd2d6822a6a2d2a26888e61f73
nyarla <nyarla@kalaclista.com>
2023-09-07 17:43:41 +0900

    refactor: rewrite WebSite::Extension::WebSite by Kalaclista::Data::WebSite

--- lib/WebSite/Extensions/WebSite.pm
+++ lib/WebSite/Extensions/WebSite.pm
@@ -3,55 +3,13 @@ package WebSite::Extensions::WebSite;
 use strict;
 use warnings;
 use utf8;
-use feature qw(state);
-
-use URI::Fast;
-use URI::Escape qw(uri_unescape);
-use Text::CSV   qw(csv);
-
-use Kalaclista::HyperScript qw(a h2 p blockquote cite div small);
 
 use Kalaclista::Constants;
-use Kalaclista::WebSite;
+use Kalaclista::Data::WebSite;
+use Kalaclista::HyperScript qw(a div h2 p cite blockquote small);
 
-my $datadir = Kalaclista::Constants->rootdir->child('content/data/webdata');
-
-sub load {
-  state $websites ||= do {
-    my $csv = csv( in => Kalaclista::Constants->rootdir->child('content/data/website.csv')->path );
-    shift $csv->@*;
-
-    my $data = {};
-    for my $line ( $csv->@* ) {
-      my $gone      = $line->[2] eq 'yes';
-      my $status    = $line->[4];
-      my $title     = $line->[5];
-      my $link      = $line->[6];
-      my $permalink = $line->[7];
-      my $summary   = $line->[8];
-
-      my $website = Kalaclista::WebSite->new(
-        is_gone      => $gone,
-        is_ignore    => $gone,
-        has_redirect => ( $link ne $permalink ),
-        status       => $status,
-        updated_at   => 0,
-        href         => $permalink,
-        title        => $title,
-        summary      => $summary,
-      );
-
-      $data->{$link}      = $website;
-      $data->{$permalink} = $website;
-    }
-
-    $data;
-  };
-
-  my $href = shift;
-
-  return $websites->{$href};
-}
+my $csv = Kalaclista::Constants->rootdir->child('content/data/website.csv')->path;
+Kalaclista::Data::WebSite->init($csv);
 
 sub transform {
   my ( $class, $entry, $dom ) = @_;
@@ -60,46 +18,32 @@ sub transform {
     my $href = $item->getAttribute('href');
     my $text = $item->innerText;
 
-    next if ( $href !~ m{^https?} );
-
-    my ( $title, $summary, $permalink, $gone );
-    my $src = uri_unescape($href);
-    utf8::decode($src);
-
-    my $website = load($href);
-    if ( defined $website && !$website->is_gone ) {
-      $title     = $website->title ne q{}   ? $website->title   : $text;
-      $summary   = $website->summary ne q{} ? $website->summary : $title;
-      $permalink = $website->href ne q{}    ? $website->href    : $href;
-      $gone      = 0;
-    }
-    else {
-      $title     = $text;
-      $permalink = $src;
-      $summary   = $title;
-      $gone      = 1;
-    }
-
-    if ( length($summary) > 39 ) {
-      $summary = substr( $summary, 0, 39 ) . "……";
-    }
+    next if $href !~ m{^https?};
 
     my $html;
-    if ( !$gone ) {
-      $html = a( { href => $permalink }, h2($title), p( cite($src) ), blockquote( p($summary) ) );
+    my $web = Kalaclista::Data::WebSite->load( text => $text, href => $href );
+
+    if ( !$web->gone ) {
+      $html = a(
+        { href => $web->permalink },
+        h2( $web->title ),
+        p( cite( $web->cite ) ),
+        blockquote( p( $web->summary ) )
+      );
     }
     else {
-      $html = div( h2($title), p( cite($src), small('（無効なリンクです）') ), blockquote( p($summary) ) );
+      $html = div(
+        h2( $web->title ),
+        p( cite( $web->cite ), small('（リンク先は見つかりません）') ),
+        blockquote( p( $web->summary ) )
+      );
     }
 
     my $article = $item->tree->createElement('aside');
     $article->setAttribute( class => 'content__card--website' );
     $article->innerHTML( $html->to_string );
-
     $item->parent->parent->replace($article);
   }
-
-  return $entry;
 }
 
 1;
