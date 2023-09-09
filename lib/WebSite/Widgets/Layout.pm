c3ee2043e79e700d6bb4e1b9badc8c4f892f6110
nyarla <nyarla@kalaclista.com>
2023-09-09 10:32:32 +0900

    refactor: use Google Analytics directly

--- lib/WebSite/Widgets/Layout.pm
+++ lib/WebSite/Widgets/Layout.pm
@@ -21,6 +21,14 @@ use WebSite::Widgets::Metadata;
 
 my $search = 'https://cse.google.com/cse?cx=018101178788962105892:toz3mvb2bhr#gsc.tab=0';
 
+my $analytics = <<'...';
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-18GLHBH79E');
+...
+
 sub layout {
   my ( $vars, $content ) = @_;
   my $baseURI = Kalaclista::Constants->baseURI;
@@ -41,6 +49,14 @@ sub layout {
       ),
       profile,
       siteinfo,
+      (
+        Kalaclista::Constants->vars->is_production
+        ? (
+          script( { async => true, src => 'https://www.googletagmanager.com/gtag/js?id=G-18GLHBH79E' }, '' ),
+          script( { defer => true },                                                                    raw($analytics) ),
+            )
+        : ()
+      ),
     ]
   );
 
