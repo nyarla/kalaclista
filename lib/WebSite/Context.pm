5437900bd97fb997750873cb4904352015afe5d2
nyarla <nyarla@kalaclista.com>
2023-11-07 09:20:56 +0900

    feat(WebSite::Context): add support to Kalaclista::Context::Environment

--- lib/WebSite/Context.pm
+++ lib/WebSite/Context.pm
@@ -14,20 +14,30 @@ class WebSite::Context : isa(Kalaclista::Context) {
     my $class  = shift;
     my $detect = shift;
 
-    my $production = ( exists $ENV{'KALACLISTA_ENV'} && $ENV{'KALACLISTA_ENV'} eq 'production' );
-    my $context    = WebSite::Context->new(
-      production => $production,
-      baseURI    => URI::Fast->new( $production ? 'https://the.kalaclista.com' : 'http://nixos:1313' ),
-      dirs       => Kalaclista::Data::Directory->instance(
+    my $stage = exists $ENV{'KALACLISTA_ENV'} ? $ENV{'KALACLISTA_ENV'} : 'development';
+    my $on    = exists $ENV{'CI'} ? 'ci' : exists $ENV{'IN_PERL_SHELL'} ? 'local' : 'runtime';
+    my $env   = Kalaclista::Context::Environment->new(
+      environment => $stage,
+      on          => $on,
+    );
+
+    my $context = WebSite::Context->new(
+      baseURI => URI::Fast->new( $env->production ? 'https://the.kalaclista.com' : 'http://nixos:1313' ),
+      dirs    => Kalaclista::Data::Directory->instance(
         detect => $detect,
         cache  => q{cache},
         dist   => q{public/dist},
         src    => q{src},
       ),
+      env => $env,
     );
 
     $class->instance($context);
     return $class->instance;
+  }
+
+  method production {
+    return $self->env->production;
   }
 
   method entries {
