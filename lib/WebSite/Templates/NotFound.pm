8fcde64720cbef2ca2e941044a3de5731fc00460
nyarla <nyarla@kalaclista.com>
2024-02-23 13:23:23 +0900

    refactor(WebSite::Templates::NotFound): apply tailwind css

--- lib/WebSite/Templates/NotFound.pm
+++ lib/WebSite/Templates/NotFound.pm
@@ -8,6 +8,7 @@ use feature qw(state);
 
 use Kalaclista::HyperScript;
 use WebSite::Helper::Hyperlink qw(href);
+use WebSite::Helper::TailwindCSS;
 
 use WebSite::Widgets::Layout;
 
@@ -18,26 +19,35 @@ sub date {
 }
 
 sub content {
-  return article(
-    { class => 'entry entry__notfound' },
-    header( h1('404 not found') ),
-    section(
-      { class => 'entry__content' },
-      hr( { class => 'sep' } ),
-      p('要求されたページは見つかりませんでした。'),
-      p('ページが見つからなかった理由には下記の様な原因があります：'),
-      ul(
-        li('歴史的経緯によりページが行方不明になっている'),
-        li('なんらかの理由によりページが削除された'),
-        li('そもそもそう言ったページは存在しない'),
-        li('URL を打ち間違えている')
-      ),
-
-      p('そのため『あれ？確か昔こんなページがあったはず……』と思われる場合、'),
-      ul( li( a( { href => $search }, 'この Web サイトの全文検索（by Google）' ) ) ),
-      p('を使うとお探しのページが見つかるかもしれません。')
+  state $header ||= header(
+    h1(
+      classes(q|text-xl sm:text-3xl font-bold mt-2 mb-4|),
+      '404 Not Found',
     ),
   );
+
+  state $content ||= section(
+    classes(q|e-content mb-4|),
+    p('要求されたページは見つかりませんでした。'),
+    p('ページが見つからなかった理由には下記の様な原因があります：'),
+    ul(
+      li('歴史的経緯によりページが行方不明になっている'),
+      li('なんらかの理由によりページが削除された'),
+      li('そもそもそう言ったページは存在しない'),
+      li('URL を打ち間違えている')
+    ),
+
+    p('そのため『あれ？確か昔こんなページがあったはず……』と思われる場合、'),
+    ul( li( a( { href => $search }, 'この Web サイトの全文検索（by Google）' ) ) ),
+    p('を使うとお探しのページが見つかるかもしれません。')
+  );
+
+  state $article ||= article(
+    $header,
+    $content,
+  );
+
+  return $article;
 }
 
 sub render {
