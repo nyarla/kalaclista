cb41609e0f9ea3b4eea944d619f42163c305414b
nyarla <nyarla@kalaclista.com>
2024-03-06 10:46:30 +0900

    refactor(WebSite::Extensions::AdjustHeading): rewrite code and test

--- lib/WebSite/Extensions/AdjustHeading.pm
+++ lib/WebSite/Extensions/AdjustHeading.pm
@@ -1,8 +1,15 @@
 package WebSite::Extensions::AdjustHeading;
 
-use strict;
-use warnings;
+use v5.38;
 use utf8;
+
+use feature qw(isa);
+
+use Exporter::Lite;
+
+use Kalaclista::HyperScript qw(strong raw);
+
+our @EXPORT_OK = qw(adjust);
 
 my %table = (
   'h1' => 'h2',
@@ -12,21 +19,30 @@ my %table = (
   'h5' => 'h6',
 );
 
+sub adjust : prototype($) {
+  my $dom = shift;
+
+  for my $node ( $dom->find('h1, h2, h3, h4, h5, h6')->@* ) {
+    if ( $node->tag ne q{h6} ) {
+      $node->tag( $table{ $node->tag } );
+      next;
+    }
+
+    my $new = $node->tree->createElement('p');
+    $new->innerHTML( strong( raw( $node->innerHTML ) )->to_string );
+
+    $node->replace($new);
+  }
+}
+
 sub transform {
   my ( $class, $entry ) = @_;
 
-  for my $node ( $entry->dom->find('h1, h2, h3, h4, h5, h6')->@* ) {
-    if ( $node->tag ne 'h6' ) {
-      my $tag = $table{ $node->tag };
-      $node->tag($tag);
-    }
-    else {
-      my $strong = $node->tree->createElement('strong');
-      $strong->innerHTML( $node->innerHTML );
-      my $new = $node->tree->createElement('p');
-      $new->appendChild($strong);
-      $node->replace($new);
-    }
+  if ( defined $entry->dom && $entry->dom isa 'HTML5::DOM::Element' ) {
+    my $dom = $entry->dom->clone(1);
+    adjust $dom;
+
+    return $entry->clone( dom => $dom );
   }
 
   return $entry;
