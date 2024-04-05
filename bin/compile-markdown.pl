c8899446914b826cc7d0c0c58f06a5e6596ac3e5
nyarla <nyarla@kalaclista.com>
2024-04-05 13:05:21 +0900

    feat(bin/compile-markdown.pl): add compile script for markdown file

---
+++ bin/compile-markdown.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+
+use v5.38;
+use utf8;
+
+use feature qw(state);
+
+use Test2::V0;
+
+use Markdown::Perl;
+
+use Kalaclista::Loader::Content qw(content);
+use Kalaclista::Path;
+
+use WebSite::Context::Path qw(srcdir);
+
+sub compile : prototype($) {
+  state $compiler ||= Markdown::Perl->new(
+    mode => 'github',
+  );
+
+  return $compiler->convert(shift);
+}
+
+sub doing {
+  my $path = shift;
+  print $path, "\n";
+
+  my $src    = srcdir->child('entries/src');
+  my $prefix = srcdir->child('entries/precompiled');
+
+  my $markdown = content $src->child($path)->path;
+  my $html     = compile $markdown;
+
+  my $out = $prefix->child($path);
+  $out->parent->mkpath;
+
+  $out->emit($html);
+
+  return 0;
+}
+
+sub testing {
+  my $markdown = <<'...';
+# Hi
+
+hello, world!
+...
+
+  subtest compile => sub {
+    my $html = compile $markdown;
+    like $html, qr{<h1>};
+    like $html, qr{<p>};
+  };
+
+  done_testing;
+}
+
+sub main { return exists $ENV{'HARNESS_ACTIVE'} ? testing(@_) : doing(@_); }
+
+main(@ARGV);
