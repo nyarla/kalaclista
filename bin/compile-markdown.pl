58c8c528a09fb09ed848d07b17156c886049bb6c
nyarla <nyarla@kalaclista.com>
2024-08-22 13:39:23 +0900

    refactor(bin/compile-markdown.pl): use worker-based markdown compiler

--- bin/compile-markdown.pl
+++ bin/compile-markdown.pl
@@ -3,60 +3,28 @@
 use v5.38;
 use utf8;
 
-use feature qw(state);
+use Parallel::Fork::BossWorkerAsync;
 
-use Test2::V0;
+use WebSite::Worker::Markdown qw(worker queues);
 
-use Markdown::Perl;
+sub main {
+  my $parallel = shift;
 
-use Kalaclista::Loader::Content qw(content);
-use Kalaclista::Path;
-
-use WebSite::Context::Path qw(srcdir);
-
-sub compile : prototype($) {
-  state $compiler ||= Markdown::Perl->new(
-    mode                   => 'github',
-    use_extended_autolinks => !!0,
+  my $bw = Parallel::Fork::BossWorkerAsync->new(
+    work_handler   => \&worker,
+    global_timeout => 5,
+    worker_count   => $parallel,
   );
 
-  return $compiler->convert(shift);
+  $bw->add_work( queues() );
+  while ( $bw->pending() ) {
+    my $result = $bw->get_result;
+    if ( exists $result->{'done'} && $result->{'done'} > 0 ) {
+      print "Compiled: " . $result->{'msg'} . ' -> ' . $result->{'dest'} . "\n";
+    }
+  }
+
+  $bw->shut_down();
 }
-
-sub doing {
-  my $path = shift;
-  print 'markdown: ', $path, "\n";
-
-  my $src    = srcdir->child('entries/src');
-  my $prefix = srcdir->child('entries/precompiled');
-
-  my $markdown = content $src->child($path)->path;
-  my $html     = compile $markdown;
-
-  my $out = $prefix->child($path);
-  $out->parent->mkpath;
-
-  $out->emit($html);
-
-  return 0;
-}
-
-sub testing {
-  my $markdown = <<'...';
-# Hi
-
-hello, world!
-...
-
-  subtest compile => sub {
-    my $html = compile $markdown;
-    like $html, qr{<h1>};
-    like $html, qr{<p>};
-  };
-
-  done_testing;
-}
-
-sub main { return exists $ENV{'HARNESS_ACTIVE'} ? testing(@_) : doing(@_); }
 
 main(@ARGV);
